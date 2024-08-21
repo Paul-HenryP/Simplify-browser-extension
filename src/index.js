f66f3d2640f3d81ff410c04ddbbbd895f687b1c0
Paul-HenryP <104301931+Paul-HenryP@users.noreply.github.com>
2024-08-21 16:09:29 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+console.log("hello world!");
+
+class ChatGPTExtension{
+    
+promptToChatGPT() {
+    const prompt = "From now on if you need to summarize text given to you from and article or a website, write a short summary of the text displayed on the website mentioning the important  aspects of that content."
+}
+
+}
